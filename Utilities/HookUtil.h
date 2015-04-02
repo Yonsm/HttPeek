6f5a97cb44627225a44d69663bd0481c2a7a7275
Yonsm <Yonsm@msn.com>
2015-04-02 22:54:13 +0800

    alwayse_inline

--- Utilities/HookUtil.h
+++ Utilities/HookUtil.h
@@ -4,23 +4,23 @@
 //
 #define _HOOK_FUNCTION_(MOD, RET, LIB, FUN, ...)		RET $##FUN(__VA_ARGS__);\
 														RET (*_##FUN)(__VA_ARGS__);\
-														void MOD _Init_##FUN() {_HookFunction(#LIB, #FUN, (void *)$##FUN, (void **)&_##FUN);}\
+														__attribute__((MOD)) void _Init_##FUN() {_HookFunction(#LIB, #FUN, (void *)$##FUN, (void **)&_##FUN);}\
 														RET $##FUN(__VA_ARGS__)
 
 #define _HOOK_MESSAGE_(MOD, RET, CLS, MSG, META, ...)	RET $##CLS##_##MSG(id self, SEL sel, ##__VA_ARGS__);\
 														RET (*_##CLS##_##MSG)(id self, SEL sel, ##__VA_ARGS__);\
-														void MOD _Init_##CLS##_##MSG() {_HookMessage(objc_get##META##Class(#CLS), #MSG, (void *)$##CLS##_##MSG, (void **)&_##CLS##_##MSG);}\
+														__attribute__((MOD)) void _Init_##CLS##_##MSG() {_HookMessage(objc_get##META##Class(#CLS), #MSG, (void *)$##CLS##_##MSG, (void **)&_##CLS##_##MSG);}\
 														RET $##CLS##_##MSG(id self, SEL sel, ##__VA_ARGS__)
 
 // 需要手动调用 _Init_*** 初始化 HOOK
-#define _HOOK_FUNCTION(RET, LIB, FUN, ...)				_HOOK_FUNCTION_(inline, RET, LIB, FUN, ##__VA_ARGS__)
-#define _HOOK_MESSAGE(RET, CLS, MSG, ...)				_HOOK_MESSAGE_(inline, RET, CLS, MSG, , ##__VA_ARGS__)
-#define _HOOK_META(RET, CLS, MSG, ...)					_HOOK_MESSAGE_(inline, RET, CLS, MSG, Meta, ##__VA_ARGS__)
+#define _HOOK_FUNCTION(RET, LIB, FUN, ...)				_HOOK_FUNCTION_(always_inline, RET, LIB, FUN, ##__VA_ARGS__)
+#define _HOOK_MESSAGE(RET, CLS, MSG, ...)				_HOOK_MESSAGE_(always_inline, RET, CLS, MSG, , ##__VA_ARGS__)
+#define _HOOK_META(RET, CLS, MSG, ...)					_HOOK_MESSAGE_(always_inline, RET, CLS, MSG, Meta, ##__VA_ARGS__)
 
 // 自动初始化 HOOK
-#define HOOK_FUNCTION(RET, LIB, FUN, ...)				_HOOK_FUNCTION_(__attribute__((constructor)), RET, LIB, FUN, ##__VA_ARGS__)
-#define HOOK_MESSAGE(RET, CLS, MSG, ...)				_HOOK_MESSAGE_(__attribute__((constructor)), RET, CLS, MSG, , ##__VA_ARGS__)
-#define HOOK_META(RET, CLS, MSG, ...)					_HOOK_MESSAGE_(__attribute__((constructor)), RET, CLS, MSG, Meta, ##__VA_ARGS__)
+#define HOOK_FUNCTION(RET, LIB, FUN, ...)				_HOOK_FUNCTION_(constructor, RET, LIB, FUN, ##__VA_ARGS__)
+#define HOOK_MESSAGE(RET, CLS, MSG, ...)				_HOOK_MESSAGE_(constructor, RET, CLS, MSG, , ##__VA_ARGS__)
+#define HOOK_META(RET, CLS, MSG, ...)					_HOOK_MESSAGE_(constructor, RET, CLS, MSG, Meta, ##__VA_ARGS__)
 
 //
 #ifdef __cplusplus
