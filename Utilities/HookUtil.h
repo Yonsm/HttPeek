f3e095f6432a5a2e48c15d5776dbb50d6bac7b19
Yonsm <Yonsm@msn.com>
2015-04-02 21:26:04 +0800

    Add HookUtil

---
+++ Utilities/HookUtil.h
@@ -0,0 +1,35 @@
+
+#import <objc/runtime.h>
+
+//
+#define _HOOK_FUNCTION_(MOD, RET, LIB, FUN, ...)		RET $##FUN(__VA_ARGS__);\
+														RET (*_##FUN)(__VA_ARGS__);\
+														void MOD _Init_##FUN() {_HookFunction(#LIB, #FUN, (void *)$##FUN, (void **)&_##FUN);}\
+														RET $##FUN(__VA_ARGS__)
+
+#define _HOOK_MESSAGE_(MOD, RET, CLS, MSG, META, ...)	RET $##CLS##_##MSG(id self, SEL sel, __VA_ARGS__);\
+														RET (*_##CLS##_##MSG)(id self, SEL sel, __VA_ARGS__);\
+														void MOD _Init_##CLS##_##MSG() {_HookMessage(objc_get##META##Class(#CLS), #MSG, (void *)$##CLS##_##MSG, (void **)&_##CLS##_##MSG);}\
+														RET $##CLS##_##MSG(id self, SEL sel, __VA_ARGS__)
+
+// 需要手动调用 _Init_*** 初始化 HOOK
+#define _HOOK_FUNCTION(RET, LIB, FUN, ...)				_HOOK_FUNCTION_(inline, RET, LIB, FUN, __VA_ARGS__)
+#define _HOOK_MESSAGE(RET, CLS, MSG, ...)				_HOOK_MESSAGE_(inline, RET, CLS, MSG, , __VA_ARGS__)
+#define _HOOK_META(RET, CLS, MSG, ...)					_HOOK_MESSAGE_(inline, RET, CLS, MSG, Meta, __VA_ARGS__)
+
+// 自动初始化 HOOK
+#define HOOK_FUNCTION(RET, LIB, FUN, ...)				_HOOK_FUNCTION_(__attribute__((constructor)), RET, LIB, FUN, __VA_ARGS__)
+#define HOOK_MESSAGE(RET, CLS, MSG, ...)				_HOOK_MESSAGE_(__attribute__((constructor)), RET, CLS, MSG, , __VA_ARGS__)
+#define HOOK_META(RET, CLS, MSG, ...)					_HOOK_MESSAGE_(__attribute__((constructor)), RET, CLS, MSG, Meta, __VA_ARGS__)
+
+//
+#ifdef __cplusplus
+extern "C"
+#endif
+void _HookFunction(const char *lib, const char *fun, void *hook, void **old);
+
+//
+#ifdef __cplusplus
+extern "C"
+#endif
+void _HookMessage(Class cls, const char *msg, void *hook, void **old);
---
+++ Utilities/HookUtil.m
@@ -0,0 +1,57 @@
+
+#import <dlfcn.h>
+#import "HookUtil.h"
+
+//
+void _HookFunction(const char *lib, const char *fun, void *hook, void **old)
+{
+	void *symbol = dlsym(dlopen(lib, RTLD_LAZY), fun);
+
+	//
+	static void (*_MSHookFunction)(void *symbol, void *hook, void **old) = NULL;
+	if (_MSHookFunction == NULL)
+	{
+		_MSHookFunction = dlsym(dlopen("/Library/Frameworks/CydiaSubstrate.framework/CydiaSubstrate", RTLD_LAZY), "MSHookFunction");
+	}
+
+	//
+	if (_MSHookFunction)
+	{
+		_MSHookFunction(symbol, hook, old);
+	}
+	else
+	{
+		*old = NULL;
+	}
+}
+
+//
+void _HookMessage(Class cls, const char *msg, void *hook, void **old)
+{
+	//
+	char name[1024];
+	int i = 0;
+	do
+	{
+		name[i] = (msg[i] == '_') ? ':' : msg[i];
+	}
+	while (msg[i]);
+	SEL sel = sel_registerName(name);
+
+	//
+	static void (*_MSHookMessageEx)(Class cls, SEL sel, void *hook, void **old) = NULL;
+	if (_MSHookMessageEx == nil)
+	{
+		_MSHookMessageEx = dlsym(dlopen("/Library/Frameworks/CydiaSubstrate.framework/CydiaSubstrate", RTLD_LAZY), "MSHookMessageEx");
+	}
+
+	//
+	if (_MSHookMessageEx)
+	{
+		_MSHookMessageEx(cls, sel, hook, old);
+	}
+	else
+	{
+		*old = method_setImplementation(class_getInstanceMethod(cls, sel), hook);
+	}
+}
